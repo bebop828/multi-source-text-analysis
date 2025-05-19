941fa8fc465df472afc7d279f630e2dbb37e3ce7
Joe Jones <bebop828jj@gmail.com>
2025-05-19 08:56:30 -0400

    Updating file

--- transcribe_assemblyAI.py
+++ transcribe_assemblyAI.py
@@ -4,7 +4,7 @@
 ############################################################
 
 ############################################################
-# last file update- 2025/05/09
+# last file update- 2025/05/19
 ############################################################
 
 import csv
@@ -13,12 +13,12 @@ import assemblyai as aai
 aai.settings.api_key = "your key goes here"
 
 transcriber = aai.Transcriber()
-transcript = transcriber.transcribe("analysis/mp3/John Denver - Take Me Home.MP3")
+transcript = transcriber.transcribe("analysis/mp3/John Denver - Take Me Home.MP3") #change this to your file path for transcrition
 
-print(transcript.text)    #display to your terminal
+print(transcript.text)    #print to your terminal
 
 # Save the transcription to a CSV file
-with open('analysis/csv transcript/Denver_countryRoad.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('analysis/csv transcript/Denver_countryRoad.csv', 'w', newline='', encoding='utf-8') as csvfile: #update to your file pathway
     writer = csv.writer(csvfile)
     # Write the transcript text as a single row
     writer.writerow([transcript.text])
